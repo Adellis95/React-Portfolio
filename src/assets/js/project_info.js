b1447f7642f8718171f62de481d5c331158c682c
Austin Ellis <Austin@Janets-iMac.local>
2021-01-30 17:03:21 -0500

    Project info with thumbnails and setup App.js

---
+++ src/assets/js/project_info.js
@@ -0,0 +1,53 @@
+import rickAndMortyAdventure from "";
+import javascriptCodeQuiz from "";
+import workDayScheduler from "";
+import weatherDash from "";
+import employeeSummary from "";
+import cyberSanta from "";
+
+const projectInfo = [
+    {
+        title: "",
+        description: '',
+        repo: "",
+        url: "",
+        image: rickAndMortyAdventure,
+    },
+    {
+        title: "",
+        description: '',
+        repo: "",
+        url: "",
+        image: javascriptCodeQuiz,
+    },
+    {
+        title: "",
+        description: '',
+        repo: "",
+        url: "",
+        image: workDayScheduler,
+    },
+    {
+        title: "",
+        description: '',
+        repo: "",
+        url: "",
+        image: weatherDash,
+    },
+    {
+        title: "",
+        description: '',
+        repo: "",
+        url: "",
+        image: employeeSummary,
+    },
+    {
+        title: "",
+        description: '',
+        repo: "",
+        url: "",
+        image: cyberSanta,
+    },
+];
+
+export default projectInfo;
